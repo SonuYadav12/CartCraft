0a81100752ad04fb193ae263b4320e9011b8d13b
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-03-25 00:53:46 +0530

    minor changes

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Item = ({ id, name, image, oldprice, newprice }) => {
   return (
-    <div key={id} className="p-2 bg-gradient-to-t from bg-gray-100 to-white rounded-xl shadow-black shadow-lg cursor-pointer w-80 flex flex-col items-center justify-center">
+    <div key={id} className="p-2 bg-white rounded-xl shadow-black shadow-lg cursor-pointer w-80 flex flex-col items-center justify-center">
       <img  className="rounded-lg" src={image} alt="" />
       <p>{name}</p>
       <div className="flex w-full items-center justify-start gap-5 p-3">
--- src/Components/NewCollection/NewCollection.jsx
+++ src/Components/NewCollection/NewCollection.jsx
@@ -4,9 +4,9 @@ import Item from '../Items/Item';
 
 const NewCollection = () => {
   return (
-    <div className='flex flex-col items-center justify-center '>
+    <div className=' flex flex-col items-center justify-cente bg-gradient-to-b from-pink-300 via-white to-white '>
     <h1 className=' text-3xl font-semibold p-2 '>NEW COLLECTIONS</h1>
-    <hr className="h-2 w-60 bg-black rounded-full mb-2" />
+    <hr className="h-2 w-60 bg-black rounded-full mb-4" />
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center'>
         {new_collection.map((item, i) => (
           <Item key={i} id={item.id} name={item.name} image={item.image} newprice={item.new_price} oldprice={item.old_price} />
