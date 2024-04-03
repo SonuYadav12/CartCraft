26fd3e3b0d597106bbf92947fc8e3f0a1c8ba153
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-04-04 01:42:11 +0530

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrum/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import ReleatedProducts from "../Components/RelatedProducts/ReleatedProducts";
 
 const Product = () => {
   const { allProduct } = useContext(ShopContext);
@@ -13,6 +14,7 @@ const Product = () => {
     <div className="flex flex-col justify-center">
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
+      <ReleatedProducts/>
     </div>
   );
 };
